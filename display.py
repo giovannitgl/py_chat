9206a410a809e4644f711e5e07fa67f404bc06a6
danilo-va <danva.comp@gmail.com>
2017-11-03 01:10:28 -0200

    Mensagem já é enviada para tela, falta melhorar design e tratar mensagens vazias

--- display.py
+++ display.py
@@ -1,47 +1,4 @@
-''' ENTER E BOTAO FUNCIONAM
 from tkinter import *
-
-def button_pressed():
-    input_get = input_field.get()
-    label = Label(frame, text=input_get)
-    input_user.set('')
-    label.pack()
-    return "break"
-
-def enter_pressed(event):
-    button_pressed()
-
-
-
-window = Tk()
-window.title('Chat')
-window.geometry("600x500")
-
-input_user = StringVar()
-input_field = Entry(window, text=input_user)
-input_field.pack(side=BOTTOM)
-
-frame = Frame(window, width=300, height=300)
-frame.pack_propagate(False) #
-input_field.bind("<Return>", enter_pressed)
-frame.pack()
-
-b = Button(window, text="Enviar", command=button_pressed)
-b.pack()
-
-window.mainloop()
-
-'''
-
-from tkinter import *
-
-def button_pressed():
-    print ("Botão")
-    return "break"
-
-def enter_pressed(event):
-    print ("Enter")
-    return
 
 class Chat(Frame):
 	def __init__(self,master=None):
@@ -54,21 +11,35 @@ class Chat(Frame):
 		bottomframe = Frame(frame)
 		bottomframe.pack(side=BOTTOM)
 
-		input_user = StringVar()
-		input_field = Entry(bottomframe, text=input_user)
-		input_field.bind("<Return>", enter_pressed)
+		self.input_user = StringVar()
+		self.input_field = Entry(bottomframe, text=self.input_user, width=80)
+		self.input_field.bind("<Return>", self.enter_pressed)
 
 		user_text = Text(leftframe,height=25,width=16)
 		user_text.configure(state=DISABLED)
-		chat_text = Text(frame,height=20,width=100)
-		chat_text.configure(state=DISABLED)
-		button = Button(bottomframe, command=button_pressed,text='send', height=3,width=5)
+		self.chat_text = Text(frame,height=23,width=100)
+		self.chat_text.configure(state=DISABLED)
+		button = Button(bottomframe, command=self.button_pressed,text='send', height=1,width=5)
 
 		user_text.pack(side=LEFT)
-		chat_text.pack(side=TOP)
-		input_field.pack(side=LEFT)
+		self.chat_text.pack(side=TOP)
+		self.input_field.pack(side=LEFT)
 		button.pack(side=RIGHT)
 		self.pack()
+
+	def button_pressed(self):
+		input_get = self.input_field.get()
+		print (input_get)	
+		if input_get != '\\n':
+			label = Label(self.chat_text, text=input_get)
+			self.input_user.set('')
+			label.pack()
+		return "break"
+
+	def enter_pressed(self,event):
+		self.button_pressed()
+		return
+
 
 if __name__ == '__main__':
 	root = Tk()
