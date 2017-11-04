63a8fb50d9f3a91bd6df7d101b005dc6ea0c4a44
danilo-va <danva.comp@gmail.com>
2017-11-04 10:54:01 -0200

    Ficou bonito, mas em progresso

--- display.py
+++ display.py
@@ -4,18 +4,27 @@ class Chat(Frame):
 	def __init__(self,master=None):
 		super().__init__(master)
 
-		self.frame = Frame(self, height=500)
+		self.frame = Frame(self)
 		self.frame.pack()
+		self.topframe = Frame(self.frame)
+		self.topframe.pack(expand=True, fill=Y)
+		self.textarea = Text(self.topframe, state=DISABLED)
+
+		self.vsb = Scrollbar(self.topframe, takefocus=0, command=self.textarea.yview)
+		self.vsb.pack(side=RIGHT, fill=Y)
+		self.textarea.pack(side=RIGHT, expand=YES, fill=BOTH)
+		self.textarea["yscrollcommand"]=self.vsb.set
+
 		leftframe = Frame(self.frame)
 		leftframe.pack(side=LEFT)
 		bottomframe = Frame(self.frame)
 		bottomframe.pack(side=BOTTOM)
 
 		self.input_user = StringVar()
-		self.input_field = Entry(bottomframe, text=self.input_user, width=80)
+		self.input_field = Entry(bottomframe, text=self.input_user, width=65)
 		self.input_field.bind("<Return>", self.enter_pressed)
 
-		user_text = Text(leftframe,height=200,width=16)
+		user_text = Text(leftframe,width=16)
 		user_text.configure(state=DISABLED)
 		button = Button(bottomframe, command=self.button_pressed,text='send', height=1,width=5)
 
@@ -23,8 +32,8 @@ class Chat(Frame):
 		seachContact = Button(leftframe, text='Send\nMessage\nto...', height=3,width=5)
 		broadcast = Button(leftframe, text='Broadcast', height=1,width=5)
 
-		seachContact.pack()
-		broadcast.pack()
+		#seachContact.pack()
+		#broadcast.pack()
 		self.input_field.pack(side=LEFT)
 		button.pack(side=RIGHT)
 		self.pack()
@@ -33,11 +42,18 @@ class Chat(Frame):
 		aux = "Me -> "
 		input_get = self.input_field.get()
 		if input_get != "":
-			input_get = aux + input_get
+			input_get = aux + input_get + "\n"
+
+			relative_position_of_scrollbar = self.vsb.get()[1]
+			self.textarea.config(state=NORMAL)
+			self.textarea.insert(END, input_get)
+			self.textarea.config(state=DISABLED)
+
 			print (input_get)	
-			label = Label(self.frame, text=input_get, fg="green", bg="white")
 			self.input_user.set('')
-			label.pack()
+
+			if relative_position_of_scrollbar == 1:
+				self.textarea.yview_moveto(1)
 		return "break"
 
 	def enter_pressed(self,event):
@@ -48,6 +64,5 @@ class Chat(Frame):
 if __name__ == '__main__':
 	root = Tk()
 	root.title('Chat')
-	root.maxsize(width=850, height=600)
 	chat = Chat(master=root)
 	chat.mainloop()
